f3cc224e5746239057673279fc56801c6cbb8d64
Urucode <mac1urucode@gmail.com>
2014-10-20 18:34:23 -0200

    add button spinner process data change appearance

--- test1/Hpple/XPathQuery.h
+++ test1/Hpple/XPathQuery.h
@@ -5,6 +5,7 @@
 //  Created by Matt Gallagher on 4/08/08.
 //  Copyright 2008 __MyCompanyName__. All rights reserved.
 //
+#import <UIKit/UIKit.h>
 
 NSArray *PerformHTMLXPathQuery(NSData *document, NSString *query);
 NSArray *PerformHTMLXPathQueryWithEncoding(NSData *document, NSString *query,NSString *encoding);
--- test1/ViewController.m
+++ test1/ViewController.m
@@ -12,15 +12,47 @@
 
 @interface ViewController ()
 @property NSURL *urlToDownload;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *downloadButton;
+@property UIActivityIndicatorView *spinner;
+@property UIImageView *spinnerView;
+@property NSString * webTitle;
+@property NSString * webImage;
+@property NSString * webDescription;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    
+    // set image to the textField
+    [self.textField setLeftViewMode:UITextFieldViewModeAlways];
+    
+    UIImageView* imgView =[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"linkImage.png"]];
+    [imgView setFrame:CGRectMake(0, self.textField.frame.size.height/2, self.textField.frame.size.height/2, self.textField.frame.size.height/4)];
+    imgView.contentMode = UIViewContentModeScaleAspectFit;
+   
+    
+    // set appeareance for the navigation bar
+    NSShadow* shadow = [NSShadow new];
+    shadow.shadowOffset = CGSizeMake(0.0f, 1.0f);
+    shadow.shadowColor = [UIColor redColor];
+    
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationItem.title = @"GUIDEPOST";
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0 green:172/255.0 blue:98/255.0 alpha:1];
+    
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor], NSFontAttributeName: [UIFont fontWithName:@"GillSans" size:25.0f]}];
+   
+    
+    
+    self.textField.leftView= imgView;
     self.urlToDownload = [[NSURL alloc] initWithString: @"https://www.facebook.com/thebeatles"];
 
 }
@@ -29,22 +61,50 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)endEditting:(id)sender {
+    UITextField *textEdit =(UITextField*) sender;
+    NSMutableString *textCompleted = [[NSMutableString alloc]initWithString:textEdit.text];
+    
+    if (!([textCompleted hasPrefix:@"http://"] || [textCompleted hasPrefix:@"https://"])){
+        [textCompleted insertString:@"http://" atIndex:0];
+    }
+    BOOL isValid = [self validateUrl:textCompleted];
+    
+    if (!isValid){
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid url" message:@"The introduced url is not valid" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil,nil];
+        [alert show];
 
+    }
+    
+}
+
+- (BOOL) validateUrl: (NSString *) candidate {
+    NSString *urlRegEx =
+    @"(http|https)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+";
+    NSPredicate *urlTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", urlRegEx];
+    return [urlTest evaluateWithObject:candidate];
+}
 
 - (IBAction)download:(id)sender {
+
+    [self.textField resignFirstResponder];
+    [self showSpinner];
     
-    NSData  * data      = [NSData dataWithContentsOfURL:self.urlToDownload];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:self.urlToDownload];
+    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:urlRequest  delegate:self];
     
-    TFHpple * doc       = [[TFHpple alloc] initWithHTMLData:data];
-    
-    NSString *xPathQueryString = @"meta[@property=\"og:title\"]/@content";
-    NSArray * elements  = [doc searchWithXPathQuery:xPathQueryString];
-    
-    for (TFHppleElement *element in elements ){
-        
-        int ;
-      //  [element]
-    }
+//    NSData  * data      = [NSData dataWithContentsOfURL:self.urlToDownload];
+//    
+//    TFHpple * doc       = [[TFHpple alloc] initWithHTMLData:data];
+//    
+//    NSString *xPathQueryString = @"//meta[@property=\"og:title\"]/@content";
+//    NSArray * elements  = [doc searchWithXPathQuery:xPathQueryString];
+//    
+//    for (TFHppleElement *element in elements ){
+//        
+//        int ;
+//      //  [element]
+//    }
    
     
     
@@ -54,6 +114,89 @@
 //    [e attributes];                 // NSDictionary of href, class, id, etc.
 //    [e objectForKey:@"href"];       // Easy access to single attribute
 //    [e firstChildWithTagName:@"b"]; // The first "b" child node
+    
+    
+}
+
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+    NSLog(@"didReceiveResponse");
+    
+}
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    NSLog(@"didReceiveData");
+    
+    TFHpple * doc       = [[TFHpple alloc] initWithHTMLData:data];
+    NSDictionary * attributes;
+    BOOL hasOG = YES;
+    
+    NSString *xPathQueryString = @"//meta[@property='og:title'] | //meta[@property='og:image'] | //meta[@property='og:description']";
+    NSArray * elements  = [doc searchWithXPathQuery:xPathQueryString];
+    
+    
+    if([elements count] < 3){
+        hasOG = NO;
+        xPathQueryString = @"//title | //meta[@name='description']";
+        elements = [doc searchWithXPathQuery:xPathQueryString];
+    }
+    
+    for (TFHppleElement *element in elements ){
+        
+        attributes = [element attributes];
+        if(hasOG){
+            
+            if([((NSString *)[attributes objectForKey:@"property"]) isEqual:@"og:title"]){
+                self.webTitle = (NSString *)[attributes objectForKey:@"content"];
+            }
+            else if([((NSString *)[attributes objectForKey:@"property"]) isEqual:@"og:image"]){
+                self.webImage = (NSString *)[attributes objectForKey:@"content"];
+            }
+            else if([((NSString *)[attributes objectForKey:@"property"]) isEqual:@"og:description"]){
+                self.webDescription = (NSString *)[attributes objectForKey:@"content"];
+            }
+        }
+        else{
+            
+            if([[element tagName] isEqual:@"title"]){
+                self.webTitle = [element text];
+            }
+            // assume that the other node is the description
+            else if([[element attributes] count] > 0){
+                
+                self.webDescription = [[element attributes] objectForKey:@"content"];
+            }
+            
+        }
+    }
+    [self stopSpinner];
+}
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    NSLog(@"connectionDidFinishLoading");
+    
+}
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    NSLog(@"didFailWithError");
+    
+    [self stopSpinner];
+    
+}
+
+-(void) showSpinner{
+    self.spinnerView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"spinnerView.png"]];
+    [self.spinnerView setFrame:self.downloadButton.bounds];
+    self.downloadButton.enabled = NO;
+    [self.downloadButton addSubview:self.spinnerView];
+    
+    
+    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.spinner.frame =self.downloadButton.bounds;
+
+    [self.downloadButton addSubview:self.spinner];
+    [self.spinner startAnimating];;
+    
+}
+
+-(void) stopSpinner{
+    [self.spinner stopAnimating];
 }
 
 @end
